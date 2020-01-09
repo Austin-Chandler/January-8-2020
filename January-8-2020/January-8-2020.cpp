b83422d3c68db3420b20fdd6f2bcc3631bd4ddf5
Austin Chandler <agcmint@gmail.com>
2020-01-09 09:57:24 -0700

    lb to kg and hexagon area

---
+++ January-8-2020/January-8-2020.cpp
@@ -0,0 +1,17 @@
+//Austin Chandler
+//This is my own work
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    double pounds = 0;
+    cout << "Please enter a number in pounds" << endl;
+    cin >> pounds;
+
+    double kilograms = pounds * 0.454;
+    cout << kilograms << endl;
+
+    return 0;
+}
